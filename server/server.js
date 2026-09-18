d254255795080dbe816af33c2b222dcd024b6c7c
garudasec <228494846+garudasec@users.noreply.github.com>
2026-09-18 14:38:41 +0000

    feat: connect backend to mongodb

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "BloodWard backend is running",
-  });
-});
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }),
+);
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
-  console.log(`BloodWard server running on port ${PORT}`);
+    console.log(`BloodWard server running on port ${PORT}`);
 });
